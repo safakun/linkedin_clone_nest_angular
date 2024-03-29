10e52373b3c2a9fb81a987cfcee8002cb799aaa6
safakun <safarovdeem@gmail.com>
2024-03-29 15:49:46 +0500

    added popover

--- linkedin/src/app/home/components/header/header.component.ts
+++ linkedin/src/app/home/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from './popover/popover.component';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
-  constructor() { }
+  constructor(public popoverController: PopoverController) { }
 
   ngOnInit() {}
+
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      cssClass: 'my-custom-class',
+      event: ev,
+      translucent: true
+    });
+    await popover.present();
+
+    const {role} = await popover.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role)
+  }
 
 }
