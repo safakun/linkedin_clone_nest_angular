054d3fb2f2153f060ccb8bd225a266e3682d17af
safakun <safarovdeem@gmail.com>
2024-04-02 10:30:47 +0500

    added response to get posts from server

--- linkedin/src/app/home/components/all-posts/all-posts.component.ts
+++ linkedin/src/app/home/components/all-posts/all-posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Post } from '../../models/Post';
 
 @Component({
   selector: 'app-all-posts',
@@ -6,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-posts.component.scss'],
 })
 export class AllPostsComponent  implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() { }
+  queryParams: string;
+  allLoadedPosts: Post[] = [];
+  numberOfPosts = 5;
+  skipPosts = 0;
 
-  ngOnInit() {}
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.getPosts(false, '');
+  }
+
+  getPosts(isInitialLoad: boolean, event: any) {
+    if (this.skipPosts === 20) {
+      event.target.disabled = true;
+    }
+    this.queryParams = `?take=${this.numberOfPosts}&skip=${this.skipPosts}`;
+
+    this.postService.getSelectedPosts(this.queryParams).subscribe((posts: Post[]) => {
+      for (let post = 0; post < posts.length; post++) {
+        this.allLoadedPosts.push(posts[post]);
+      }
+      if (isInitialLoad) event.target.complete;
+      this.skipPosts = this.skipPosts + 5;
+    }, (error) => {
+      console.log(error);
+    })
+  }
+
+  loadData(event: any) {
+    this.getPosts(true, event);
+  }
 
 }
