3a47d8ec4c4720afd4f552d3e66bf830922dad46
safakun <safarovdeem@gmail.com>
2024-04-20 17:46:48 +0500

    added new func to get user on user commection component angular

--- linkedin/src/app/home/components/connection-profile/connection-profile.component.ts
+++ linkedin/src/app/home/components/connection-profile/connection-profile.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { BannerColorService } from '../../services/banner-color.service';
+import { BehaviorSubject, Observable, Subscription, map, switchMap, take } from 'rxjs';
+import { Role, User } from 'src/app/auth/models/user.model';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { ConnectionProfileService } from '../../services/connection-profile.service';
 
 @Component({
   selector: 'app-connection-profile',
   templateUrl: './connection-profile.component.html',
   styleUrls: ['./connection-profile.component.scss'],
 })
-export class ConnectionProfileComponent  implements OnInit {
+export class ConnectionProfileComponent  implements OnInit, OnDestroy {
 
-  constructor() { }
+ 
 
-  ngOnInit() {}
+  constructor(
+    public bannerColorService: BannerColorService,
+    private route: ActivatedRoute,
+    private connectionProfileService: ConnectionProfileService
+  ) { }
 
-}
+  ngOnInit() {
+    this.getUser().subscribe((x) => console.log(x))
+   // this.getUserIdFromUrl().subscribe((x) => console.log(33, x));
+  }
+
+  getUser(): Observable<User> {
+    return this.getUserIdFromUrl().pipe(
+      switchMap((userId: number) => {
+        return this.connectionProfileService.getConnectionYser(userId)
+      })
+    )
+  }
+
+   private getUserIdFromUrl(): Observable<number> {
+    return this.route.url.pipe(
+      map((urlSegment: UrlSegment[]) => {
+        return Number(urlSegment[0].path);
+      })
+    )
+   }
+
+  
+   ngOnDestroy(): void {
+       
+   }
+  
+  }
+
+ 
+
--- linkedin/src/app/home/services/connection-profile.service.ts
+++ linkedin/src/app/home/services/connection-profile.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/auth/models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionProfileService {
+  private httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getConnectionYser(id: number): Observable<User> {
+    return this.http.get<User>(`${environment.baseApiUrl}/user/${id}`);
+  }
 }
