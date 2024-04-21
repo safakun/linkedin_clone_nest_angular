b661317bc4943739ec871f29f0dc86cd1b45e1bb
safakun <safarovdeem@gmail.com>
2024-04-21 09:01:12 +0500

    added funcs for one single user angular

--- linkedin/src/app/home/components/connection-profile/connection-profile.component.ts
+++ linkedin/src/app/home/components/connection-profile/connection-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { BannerColorService } from '../../services/banner-color.service';
-import { BehaviorSubject, Observable, Subscription, map, switchMap, take } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, map, switchMap, take, tap } from 'rxjs';
 import { Role, User } from 'src/app/auth/models/user.model';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { ConnectionProfileService } from '../../services/connection-profile.service';
+import { FriendRequestStatus, FriendRequest_Status } from '../../models/FriendRequest';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-connection-profile',
@@ -13,6 +15,10 @@ import { ConnectionProfileService } from '../../services/connection-profile.serv
 })
 export class ConnectionProfileComponent  implements OnInit, OnDestroy {
 
+  user: User;
+  friendRequestStatus: FriendRequest_Status;
+  friendRequestStatusSubscription$: Subscription;
+  userSubscription$: Subscription;
  
 
   constructor(
@@ -22,6 +28,16 @@ export class ConnectionProfileComponent  implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
+    this.friendRequestStatusSubscription$ = this.getFriendRequestStatus().pipe(
+      tap((friendRequestStatus: FriendRequestStatus) => {
+        this.friendRequestStatus = friendRequestStatus.status;
+        this.userSubscription$ = this.getUser().subscribe((user: User) => {
+          this.user = user;
+          const imgPath = user.imagePath ?? 'blank-profile-picture.png';
+          this.user['fullImagePath'] = `${environment.baseApiUrl}/feed/image/${imgPath}`;
+        })
+      })
+    ).subscribe();
     this.getUser().subscribe((x) => console.log(x))
    // this.getUserIdFromUrl().subscribe((x) => console.log(33, x));
   }
@@ -34,6 +50,19 @@ export class ConnectionProfileComponent  implements OnInit, OnDestroy {
     )
   }
 
+  getFriendRequestStatus(): Observable<FriendRequestStatus> {
+    return this.getUserIdFromUrl().pipe(
+      switchMap((userId: number) => {
+        return this.connectionProfileService.getFriendRequestStatus(userId);
+      })
+    )
+  }
+
+  ngOnDestroy(): void {
+       this.userSubscription$.unsubscribe();
+       this.friendRequestStatusSubscription$.unsubscribe();
+  }
+
    private getUserIdFromUrl(): Observable<number> {
     return this.route.url.pipe(
       map((urlSegment: UrlSegment[]) => {
@@ -43,9 +72,7 @@ export class ConnectionProfileComponent  implements OnInit, OnDestroy {
    }
 
   
-   ngOnDestroy(): void {
-       
-   }
+  
   
   }
 
--- linkedin/src/app/home/services/connection-profile.service.ts
+++ linkedin/src/app/home/services/connection-profile.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/auth/models/user.model';
 import { environment } from 'src/environments/environment';
+import { FriendRequestStatus } from '../models/FriendRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class ConnectionProfileService {
 
   getConnectionYser(id: number): Observable<User> {
     return this.http.get<User>(`${environment.baseApiUrl}/user/${id}`);
+  }
+
+  getFriendRequestStatus(id: number): Observable<FriendRequestStatus>  {
+    return this.http.get<FriendRequestStatus>(`${environment.baseApiUrl}/user/friend-request/status/${id}`);
   }
 }
